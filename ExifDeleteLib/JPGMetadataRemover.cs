5c03547b6ff29cd113773710f146ed9247de0bb4
SenseiMori <mtpchelokot@gmail.com>
2025-04-25 20:43:46 +0500

    Добавлен класс, отвечающий за выполнение удаления exif

---
+++ ExifDeleteLib/JPGMetadataRemover.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace ExifDeleteLib
+{
+    public class JPGMetadataRemover
+    {
+        JPGFile JPGFile { get; set; }
+        JPGDirectory JPGDirectory { get; set; }
+
+        public void RemoveExifForImage(string [] files)
+        {
+            foreach (string image in files)
+            {
+                byte[] clearData = JPGFile.FindMarkers(image);
+                string clearImagesDirectory = JPGDirectory.CreateDirectory(image);
+                string clearImage = JPGDirectory.CreateFileInNewDirectory(clearImagesDirectory);
+                JPGDirectory.WriteDataToNewFile(clearData, clearImage);
+            }
+        }
+    }
+}
---
+++ ExifDeleteLib/ZipCreator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.IO;
+using System.Text;
+
+namespace ExifDeleteLib
+{
+    public class ZipCreator
+    {
+        public void CreateZip(string[] inputFiles, string newZipName, string resultDirectory)
+        {
+            {
+                string zipPath = Path.Combine(resultDirectory, newZipName);
+                using var outStream = new FileStream(zipPath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None, 4096, true);
+                using var archive = new ZipArchive(outStream, ZipArchiveMode.Create, true);
+                foreach (string fileName in inputFiles)
+                {
+                    using (var inputStream = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read, 4096, true))
+                    {
+                        ZipArchiveEntry fileEntry = archive.CreateEntry(Path.GetFileName(fileName));
+                        using var entryStream = fileEntry.Open();
+                        inputStream.CopyTo(entryStream);
+
+                    }
+                }
+            }
+        }
+    }
+}
